f22e47448e03fa64b5c0bfa0b028f4b6213c27f6
ngallo1 <ngallo1@protonmail.com>
2025-04-15 16:08:37 -0500

    streamilt app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,85 +1,33 @@
 import streamlit as st
-import pickle
 
 st.set_page_config(
     page_title="Compare Parsers",
     layout="wide"
 )
- 
-st.header("Compare PDF Parse")
+# st.logo("images/horizontal_blue.png", icon_image="images/icon_blue.png")
 
-st.write("Compare the PDF parse of different parsers")
+home_page = st.Page(
+    page="src/streamlit/home.py",
+    title="Home",
+    icon=":material/home:"
+)
 
-# ---------   Data 
-@st.cache_data
-def load_parsed_documents():
-    """Load documents already parsed with different parsers."""
-    import pickle
+# Define data and model
+compare_page = st.Page(
+    page="src/streamlit/compare.py",
+    title="Compare Parsers",
+    icon=":material/table_edit:"
+)
 
-    pickle_filenames = {
-        'Docling': './data/docling_docs.pkl',
-        'Mistral': './data/mistral_docs.pkl',
-        'Llama': './data/llama_docs.pkl',
-    }
+pages = {
+    "Home":
+        [home_page],
+    "Compare":
+        [compare_page],
+}
 
-    parsed_documents = {}
-    for parser_name, filename in pickle_filenames.items():
-        with open(filename, 'rb') as f:
-            parsed_documents[parser_name] = pickle.load(f)
-
-    return parsed_documents
-
-
-parsed_documents = load_parsed_documents()
-parsers = list(parsed_documents.keys())
-# TODO: ensure all are the same
-DB_DOCUMENT_NAMES = parsed_documents[parsers[0]].keys()
-
-def get_different_parses_of_document(document_name, as_markdown=False):
-    def get_markdown_llama(document):
-        return "\n\n".join(page.text for page in document)
-    
-    def get_markdown_mistral(ocr_response):
-        return "\n\n".join([page.markdown for page in ocr_response.pages])
-    
-    def get_markdown_docling(document):
-        return document.export_to_markdown()
-
-    markdown_funcs = {
-        "Llama": get_markdown_llama,
-        "Mistral": get_markdown_mistral,
-        "Docling": get_markdown_docling,
-    }
-
-    out = {}
-    for parser in parsers:
-        out[parser] = parsed_documents[parser][document_name]
-        if as_markdown:
-            out[parser] = markdown_funcs[parser](out[parser])
-
-    return out
-
-with st.sidebar:
-    document_name = st.selectbox("Document Name", options=DB_DOCUMENT_NAMES)
-    parser = st.selectbox("Parser", options=parsers)
-
-markdowns = get_different_parses_of_document(document_name, as_markdown=True)
- 
-# tabs = st.tabs(parsers)
-# for parser, tab in zip(parsers, tabs):
-#     st.markdown(markdowns[parser])
-st.divider()
-st.markdown(markdowns[parser])
-
-# print(tabs[0])
-
-#print(documents)
-# st.write(document_name)
-
-
-
-# st.write(DB_DOCUMENT_NAMES)
-# st.write(parsed_documents[parser].keys() for parser in parsers)
-
-
-
+pg = st.navigation(
+    pages=pages,
+    expanded=True,
+)
+pg.run()
